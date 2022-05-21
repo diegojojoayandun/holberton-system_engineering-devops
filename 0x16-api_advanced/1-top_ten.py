377c4f685dbc82806553a7aa191c325ad55f01bf
diegojojoayandun <3922@holbertonschool.com>
2022-05-21 05:51:58 -0500

    Add task'1. Top Ten'

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Modules to get the  the titles of the first 10 hot posts
+listed for Reddit subreddit."
+"""
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    options = {'allow_redirects': False}
+
+    try:
+        response = requests.get(url=url,
+                                headers=headers,
+                                allow_redirects=options)
+
+        for _item in response.json().get('data').get('children'):
+            print(_item.get('data').get('title'))
+
+    except Exception:
+        return 0
